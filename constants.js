cee05af2c16aaef729da3c5d0647a08ca3b588f0
shareming7 <2395277764@qq.com>
2016-12-12 16:39:00 +0800

    update constants.js

--- constants.js
+++ constants.js
@@ -4,8 +4,8 @@ exports.IN_DEVELOPMENT = ('development' == process.env.NODE_ENV);
 exports.NOT_IN_PRODUCTION = ('production' != process.env.NODE_ENV);
 
 exports.RET_CODE = {
-  OK: 1000,
-  FAILURE: 1001,
+	OK: 1000,
+	FAILURE: 1001,
 };
 
 exports.ROUTE_PATHS = {
@@ -20,10 +20,10 @@ exports.ROUTE_PARAMS = {
 };
 
 exports.LANG = {
-  EN_GB: {
-    TITLE: {
-      APP_NAME: "ExpressAndReactDemo",
-      ADMIN_PAGE: "Admin Page",
+	EN_GB: {
+		TITLE: {
+			APP_NAME: "ExpressAndReactDemo",
+			ADMIN_PAGE: "Admin Page",
 		}
 	}
 }
